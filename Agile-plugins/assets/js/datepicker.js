f8aa8de6814c354daf1082268b1f242f74ca472f
Pietro Biffi <sidbiffi@gmail.com>
2019-07-24 09:43:52 +0200

    Delete datepicker.js

--- Agile-plugins/assets/js/datepicker.js
+++
@@ -1,9 +0,0 @@
-/*
- * Attaches the image uploader to the input field
- */
-jQuery(document).ready(function($){
-
-   $( "#datepicker" ).datepicker({ dateFormat: 'dd/mm/yy' });
-   $( "#datepicker2" ).datepicker({ dateFormat: 'dd/mm/yy' });
-
-});
