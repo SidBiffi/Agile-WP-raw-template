7b35be79a7efeb00d566bfde0151777ce3cda2e6
Pietro Biffi <sidbiffi@gmail.com>
2019-07-25 14:03:44 +0200

    Update functions.php

--- Agile/functions.php
+++ Agile/functions.php
@@ -6,10 +6,8 @@
 		function theme_name_scripts() {
 
 			//jquery-bootstrap
-			wp_enqueue_script( 'jquery', get_stylesheet_directory_uri() . '/js/jquery.js' );
-		    wp_enqueue_script( 'bootstrap', get_stylesheet_directory_uri() . '/js/bootstrap.min.js' );
-		
-		    //wp_enqueue_script( 'yourscriptname', get_stylesheet_directory_uri() . '/js/yourscriptname.js'  );
+			wp_enqueue_script( 'jquery', get_stylesheet_directory_uri() . '/js/jquery.js' );		    		
+			//wp_enqueue_script( 'yourscriptname', get_stylesheet_directory_uri() . '/js/yourscriptname.js'  );
 
 		}
 
