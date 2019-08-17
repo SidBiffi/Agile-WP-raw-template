69b26fefe8bd50ea3880676f2150a2bab3d2a3d2
Pietro Biffi <sidbiffi@gmail.com>
2019-08-18 00:13:45 +0200

    Update index.php

--- Agile/index.php
+++ Agile/index.php
@@ -7,7 +7,6 @@
           <?php $loop = new WP_Query( array( 'post_type' => 'post', 'posts_per_page' => -1 ) ); ?>
           <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
 
-						<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="thumbnail">
             <h2><a href = "<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
             <p class="tesxt-muted"><?php the_author(); ?> il <?php the_time('jS F, Y'); ?></p> 							
             <p><?php the_excerpt(''); ?></p> 	
