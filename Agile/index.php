c24b7ef6bb12c2b30a855bf447ed2917821221a9
Pietro Biffi <sidbiffi@gmail.com>
2019-08-18 00:12:42 +0200

    Update index.php

--- Agile/index.php
+++ Agile/index.php
@@ -1,25 +1,21 @@
 <?php get_header(); ?>
 	
-				<div class = "row">
-					<div class = "col-md-9">
-						<div class = "panel panel-default panel-body">
+  <main class="container">
+    <div class="row">
+      <div class="col-12">
 
-							<?php $loop = new WP_Query( array( 'post_type' => 'post', 'posts_per_page' => -1 ) ); ?>
-							<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+          <?php $loop = new WP_Query( array( 'post_type' => 'post', 'posts_per_page' => -1 ) ); ?>
+          <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
 
-								<h2><a href = "<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-								<p = "tesxt-muted"><?php the_author(); ?> il <?php the_time('jS F, Y'); ?></p> 							
-								<p><?php the_excerpt(''); ?></p> 	
+						<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="thumbnail">
+            <h2><a href = "<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <p class="tesxt-muted"><?php the_author(); ?> il <?php the_time('jS F, Y'); ?></p> 							
+            <p><?php the_excerpt(''); ?></p> 	
 
-							<?php endwhile; wp_reset_query(); ?>
-
-						</div>
-					</div>
-					<div class = "col-md-3">
-
-						<?php get_sidebar(); ?>
-
-					</div>
-				</div>
+          <?php endwhile; wp_reset_query(); ?>
+          
+      </div>
+    </div>
+  </main>
 
 <?php get_footer(); ?>
